53c945729b345b5a17bb95985683a5148709f4c5
Lone <salman.lone@monster.com>
2019-10-17 14:16:20 +0800

    Grid aligned

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { GridviewComponent } from './gridview/gridview.component';
 
 
 const routes: Routes = [
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,46 +6,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  private gridApi;
+  private gridColumnApi;
+
   constructor() { }
+
+  @ViewChild('agGrid') agGrid;
 
   ngOnInit() {
   }
 
   title = 'grid';
 
-  columnDefs = [
-    {headerName: 'Product', field: 'Product', filter: "agTextColumnFilter",
-      filterParams: {
-        filterOptions: ["contains", "notContains"],
-        textFormatter: function(r) {
-          if (r == null) return null;
-          r = r.replace(new RegExp("[àáâãäå]", "g"), "a");
-          r = r.replace(new RegExp("æ", "g"), "ae");
-          r = r.replace(new RegExp("ç", "g"), "c");
-          r = r.replace(new RegExp("[èéêë]", "g"), "e");
-          r = r.replace(new RegExp("[ìíîï]", "g"), "i");
-          r = r.replace(new RegExp("ñ", "g"), "n");
-          r = r.replace(new RegExp("[òóôõøö]", "g"), "o");
-          r = r.replace(new RegExp("œ", "g"), "oe");
-          r = r.replace(new RegExp("[ùúûü]", "g"), "u");
-          r = r.replace(new RegExp("[ýÿ]", "g"), "y");
-          return r;
-        },
-        debounceMs: 0,
-        caseSensitive: true,
-        suppressAndOrCondition: true}},
-    {headerName: 'Supplement ', field: 'Supplement', sortable: true, filter: true},
-    {headerName: 'Status', field: 'Status', filter: true},
-    {headerName: 'Type ', field: 'Type', filter: true, sortable: true},
-    {headerName: 'Packaging', field: 'Packaging', filter: true, sortable: true},
-    {headerName: 'Date created/last updated', field: 'DateCreatedLastUpdated', sortable: true},
-    {headerName: 'Action', field: 'Action'}
-];
+    columnDefs = [
+      {headerName: 'Product', field: 'Product', filter: "agTextColumnFilter", resizable: true, minWidth: 150,
+        filterParams: {
+          filterOptions: ["contains", "notContains"],
+          textFormatter: function(r) {
+            if (r == null) return null;
+            r = r.replace(new RegExp("[àáâãäå]", "g"), "a");
+            r = r.replace(new RegExp("æ", "g"), "ae");
+            r = r.replace(new RegExp("ç", "g"), "c");
+            r = r.replace(new RegExp("[èéêë]", "g"), "e");
+            r = r.replace(new RegExp("[ìíîï]", "g"), "i");
+            r = r.replace(new RegExp("ñ", "g"), "n");
+            r = r.replace(new RegExp("[òóôõøö]", "g"), "o");
+            r = r.replace(new RegExp("œ", "g"), "oe");
+            r = r.replace(new RegExp("[ùúûü]", "g"), "u");
+            r = r.replace(new RegExp("[ýÿ]", "g"), "y");
+            return r;
+          },
+          debounceMs: 0,
+          caseSensitive: true,
+          suppressAndOrCondition: true}},
+      {headerName: 'Supplement ', field: 'Supplement', sortable: true, filter: true, resizable: true, minWidth: 150},
+      {headerName: 'Status', field: 'Status', filter: true, resizable: true, minWidth: 150},
+      {headerName: 'Type ', field: 'Type', filter: true, sortable: true, resizable: true, minWidth: 150},
+      {headerName: 'Packaging', field: 'Packaging', filter: true, sortable: true, resizable: true, minWidth: 150},
+      {headerName: 'Date created/last updated', field: 'DateCreatedLastUpdated', sortable: true, resizable: true, minWidth: 150},
+      {headerName: 'Action', field: 'Action', minWidth: 150}
+  ];
 
-rowData = [
-  {Product: 'Toyota', Supplement: 'Celica', Status: 35000, Type: 35000, Packaging: 35000, DateCreatedLastUpdated: 35000, Action: 35000},
-  {Product: 'Ford', Supplement: 'Mondeo', Status: 32000, Type: 35000, Packaging: 35000, DateCreatedLastUpdated: 35000, Action: 35000},
-  {Product: 'Porsche', Supplement: 'Boxter', Status: 72000, Type: 35000, Packaging: 35000, DateCreatedLastUpdated: 35000, Action: 35000}
-];
+  rowData = [
+    {Product: 'Toyota', Supplement: 'Celica', Status: 35000, Type: 35000, Packaging: 35000, DateCreatedLastUpdated: 35000, Action: 35000},
+    {Product: 'Ford', Supplement: 'Mondeo', Status: 32000, Type: 35000, Packaging: 35000, DateCreatedLastUpdated: 35000, Action: 35000},
+    {Product: 'Porsche', Supplement: 'Boxter', Status: 72000, Type: 35000, Packaging: 35000, DateCreatedLastUpdated: 35000, Action: 35000}
+  ];
+
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+
+    params.api.sizeColumnsToFit();
+
+    params.api.sizeColumnsToFit();
+    window.addEventListener("resize", function() {
+      setTimeout(function() {
+        params.api.sizeColumnsToFit();
+      });
+    });
+
+    console.log('btSizeColsToFix ');
+  }
 }
