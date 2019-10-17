b8dd6dd9711c255f315e3dc507da4fd4977ea95d
unknown <salmanlone89@gmail.com>
2019-10-17 06:03:57 +0500

    search box for columns data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,26 @@ export class AppComponent {
   title = 'grid';
 
   columnDefs = [
-    {headerName: 'Product', field: 'Product', sortable: true, filter: true},
+    {headerName: 'Product', field: 'Product', filter: "agTextColumnFilter",
+      filterParams: {
+        filterOptions: ["contains", "notContains"],
+        textFormatter: function(r) {
+          if (r == null) return null;
+          r = r.replace(new RegExp("[àáâãäå]", "g"), "a");
+          r = r.replace(new RegExp("æ", "g"), "ae");
+          r = r.replace(new RegExp("ç", "g"), "c");
+          r = r.replace(new RegExp("[èéêë]", "g"), "e");
+          r = r.replace(new RegExp("[ìíîï]", "g"), "i");
+          r = r.replace(new RegExp("ñ", "g"), "n");
+          r = r.replace(new RegExp("[òóôõøö]", "g"), "o");
+          r = r.replace(new RegExp("œ", "g"), "oe");
+          r = r.replace(new RegExp("[ùúûü]", "g"), "u");
+          r = r.replace(new RegExp("[ýÿ]", "g"), "y");
+          return r;
+        },
+        debounceMs: 0,
+        caseSensitive: true,
+        suppressAndOrCondition: true}},
     {headerName: 'Supplement ', field: 'Supplement', sortable: true, filter: true},
     {headerName: 'Status', field: 'Status', filter: true},
     {headerName: 'Type ', field: 'Type', filter: true, sortable: true},
